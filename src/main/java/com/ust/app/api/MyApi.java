e3d561eaaabeca331ae7b82dfed06de244a2860b
Rakesh1723 <131934842+Rakesh1723@users.noreply.github.com>
2024-10-25 12:39:26 +0530

    second commit

--- src/main/java/com/ust/app/api/MyApi.java
+++ src/main/java/com/ust/app/api/MyApi.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyApi {
     @GetMapping
     public String sayHello(){
-        return "Hello World!";
+        return "Hello World! welcome to ust global";
     }
 }
